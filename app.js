e7f3f37e672d120a2896a9c9c5db9decbfea4f92
Akash1520 <CE2020_107079@Ldceahm.gujgov.edu.in>
2022-07-20 20:59:09 +0530

    last commit hopefully

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get("/",(req,res)=>{
 })
 
 app.post("/",(req,res)=>{
-    const url="https://api.openweathermap.org/data/2.5/weather?q="+req.body.city+"&appid="+apikey+"&units="+req.body.unit+"&lang="+req.body.lang
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+req.body.city+"&appid=${apikey}"&units="+req.body.unit+"&lang="+req.body.lang
     https.get(url,(response)=>{
         console.log(response.statusCode)
         response.on("data",(data)=>{
