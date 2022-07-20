c1cb4a86cd52f02ce548ca81cbaa9531c776e953
Akash1520 <CE2020_107079@Ldceahm.gujgov.edu.in>
2022-07-20 19:57:20 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// jshint esversion: 6
+const express = require ("express")
+const https = require("https")
+const bodyParser= require("body-parser")
+const app = express()
+const apikey="1ce09f18fc50603c69e8c1dff6d5c9e0"
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html")
+    
+})
+
+app.post("/",(req,res)=>{
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+req.body.city+"&appid="+apikey+"&units="+req.body.unit+"&lang="+req.body.lang
+    https.get(url,(response)=>{
+        console.log(response.statusCode)
+        response.on("data",(data)=>{
+            const weatherData = JSON.parse(data)
+            const icon="http://openweathermap.org/img/wn/"+weatherData.weather[0].icon+"@2x.png"
+            console.log(weatherData.main.temp)
+            
+            res.write("<h1>Today's weather : " + weatherData.weather[0].description + "</br>"+" and temprature will be " + weatherData.main.temp + " degrees</h1>")
+            res.write("<img src="+icon+">")
+            res.send()
+        })
+    })
+})
+
+
+
+
+app.listen(3000,()=>{console.log("Server is running.")})
